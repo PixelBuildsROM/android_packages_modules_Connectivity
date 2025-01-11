eeb8fe645a610addac159ff05cc287eb85666040
ExactExampl <xmpl@dontsleep.work>
2025-01-11 12:54:11 +0200

    Drop nonexistent annotations
    Change-Id: I2503d601c08c0effc9de9ed3f518947f25201aac

--- service/src/com/android/server/BpfNetMaps.java
+++ service/src/com/android/server/BpfNetMaps.java
@@ -977,7 +977,6 @@ public class BpfNetMaps {
      * @param address target address to set the ingress discard rule
      * @param iface allowed interface
      */
-    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     public void setIngressDiscardRule(final InetAddress address, final String iface) {
         throwIfPreT("setIngressDiscardRule is not available on pre-T devices");
         final int ifIndex = mDeps.getIfIndex(iface);
@@ -1000,7 +999,6 @@ public class BpfNetMaps {
      *
      * @param address target address to remove the ingress discard rule
      */
-    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     public void removeIngressDiscardRule(final InetAddress address) {
         throwIfPreT("removeIngressDiscardRule is not available on pre-T devices");
         try {
